921ed1e8af9f1f7ae404de381c59224631b90b78
Animatrix3253 <wpcf160@gmail.com>
2025-01-22 11:31:09 -0600

    Added Neo-Tree plugin

--- init.lua
+++ init.lua
@@ -11,6 +11,10 @@ require("config.lazy")
 require("tokyonight")
 vim.cmd.colorscheme "tokyonight-night" 
 
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+
 local builtin = require("telescope.builtin")
 vim.keymap.set('n','<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -10,6 +10,16 @@ return {
     {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+      }
+    },
 
 }
