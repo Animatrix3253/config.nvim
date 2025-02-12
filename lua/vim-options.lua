a0876a8834908982d333f99014a74cb84b1982b7
Animatrix3253 <wpcf160@gmail.com>
2025-02-12 12:57:04 -0600

    Added terminal binding: "ctrl t"

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,9 @@
 vim.cmd.set("expandtab")
-vim.cmd.set("tabstop=2")
-vim.cmd.set("softtabstop=2")
-vim.cmd.set("shiftwidth=2")
+vim.cmd.set("tabstop=4")
+vim.cmd.set("softtabstop=4")
+vim.cmd.set("shiftwidth=4")
 vim.cmd.set("number")
 vim.cmd.set("relativenumber")
 vim.g.leader = " "
 
-
+vim.keymap.set("n", "<C-t>", ":split | :term<CR>", {noremap = true, silent = true})
