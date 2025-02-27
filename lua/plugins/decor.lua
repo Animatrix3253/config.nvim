8168b345c6079f5ca03290fa558638290b1f9cc8
Animatrix3253 <wpcf160@gmail.com>
2025-02-26 20:04:03 -0600

    combined onedark amd dashboard into one file

--- lua/plugins/decor.lua
+++ lua/plugins/decor.lua
@@ -1,21 +1,45 @@
 return {
-    "goolord/alpha-nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.startify")
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.startify")
 
-        dashboard.section.header.val = {
+            dashboard.section.header.val = {
  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
-}
+            }
 
-        alpha.setup(dashboard.opts)
-    end
+            alpha.setup(dashboard.opts)
+        end
+    },
+
+       
+    {
+       "navarasu/onedark.nvim", 
+       priority = 1000, 
+       config = function()
+          require('onedark').setup {
+               style = "deep",
+
+               code_style = {
+                   comments =  "bold,underline",
+                   keywords =  "none",
+                   functions = "bold",
+                   strings =   "bold",
+                   variables = "none",
+               },
+
+           }
+           
+           require('onedark').load()
+       end
+    }
 
 }
